9b6f5a2f0b9738aec6dbb08598f38547a9a57dca
ayimdomnic <ayimdomnic@gmail.com>
2019-04-15 13:43:22 +0300

    [#009](Feature)- Added the Auth and Profile pages

---
+++ src/components/resturants/Resturants.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Resturants"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/resturants/ShowResturant.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ShowResturant"
+    }
+</script>
+
+<style scoped>
+
+</style>
